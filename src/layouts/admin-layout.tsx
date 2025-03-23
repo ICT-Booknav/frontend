144be65f466fb732fb3af1ca22d0c2764bbcf8dc
Rael <emilyekos@gmail.com>
2025-03-23 18:15:03 +0900

    feat: admin용 네비게이션바 추가

--- src/layouts/admin-layout.tsx
+++ src/layouts/admin-layout.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from 'react-router-dom';
 import styled from 'styled-components';
 import Footer from '@components/Footer/footer';
+import AdminNavigationBar from '@components/Navbar/admin-navbar';
 
 const AdminLayout: React.FC = () => {
 
   return (
     <Layout>
+      <AdminNavigationBar />
       <Main>
         <Outlet />
       </Main>
@@ -24,5 +26,5 @@ const Layout = styled.div`
 
 const Main = styled.main`
   flex: 1;
-  margin-top: 80px;
+  margin-top: 70px;
 `;
